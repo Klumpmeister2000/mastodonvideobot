f4da08e7897825e31098cd3e073ba9f1caeec502
Klumpmeister2000 <christian@klumpmeister.net>
2023-03-16 10:46:48 -0400

    full script ready for publishing

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# mastodonvideobot
-python script that takes a file path and outputs a video clip for posting on mastodon
+Mastodon bot
+
+- Inputs video file from folder path
+- clips random video with into a random 50 second clip
+- posts to mastodon
+- 30 second wait for mastodon to catch up on uploading
+- finished
---
+++ clipscript.py
@@ -0,0 +1,43 @@
+import os
+import random
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.audio.io.AudioFileClip import AudioFileClip
+
+# Set the folder path where the video files are located
+folder_path = "/filepath"
+
+# Set the output file name
+output_file_name = "randomnewclip.mp4"
+
+# Set the duration of the clip
+duration = 50
+
+# Get a list of all the video files in the folder
+video_files = [f for f in os.listdir(folder_path) if f.endswith((".mp4", ".avi", ".mov", ".mkv"))]
+
+# Choose a random video file from the list
+random_video_file = random.choice(video_files)
+
+# Create a VideoFileClip object from the random video file
+video = VideoFileClip(os.path.join(folder_path, random_video_file))
+
+# Get the duration of the video
+video_duration = video.duration
+
+# Choose a random start time for the clip
+start_time = random.uniform(0, video_duration - duration)
+
+# Set the end time of the clip
+end_time = start_time + duration
+
+# Clip the video
+clip = video.subclip(start_time, end_time)
+
+# Get the audio from the video
+audio = clip.audio
+
+# Write the clip to a new file with the audio
+clip_with_audio = clip.set_audio(audio)
+clip_with_audio.write_videofile(os.path.join(folder_path, output_file_name))
+
+print("The new video file has been created successfully!")
